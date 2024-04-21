47ba930238e09ba071ace63e5cc1abd586337696
Lucas Fragoso <88888171+lu-fragoso@users.noreply.github.com>
2024-04-21 17:36:23 -0300

    Refatorar App.js para adicionar funcionalidade de navegação

--- Estoque/App.js
+++ Estoque/App.js
@@ -1,97 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Image, TextInput, View, StyleSheet } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import Constants from 'expo-constants';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Produto from './view/produto/produto';
+import CadastroUsuario from './view/usuario/usuario';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  const [produto, setProduto] = useState('');
-  const [descricao, setDescricao] = useState('');
-  const [valor, setValor] = useState('');
-  const [foto, setFoto] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      if (Constants.platform.ios) {
-        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-        if (status !== 'granted') {
-          alert('Desculpe, precisamos de permissões de câmera para fazer isso funcionar!');
-        }
-      }
-    })();
-  }, []);
-
-  const escolherFotoDaGaleria = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    console.log(result);
-
-    if (!result.canceled) {
-      setFoto(result.assets[0].uri);
-    }
-  };
-  
-
-  const cadastrarProduto = () => {
-    // Aqui você pode adicionar a lógica para cadastrar o produto
-    console.log(`Produto: ${produto}, Descrição: ${descricao}, Valor: ${valor}, Foto: ${foto}`);
-
-    setProduto('')
-    setDescricao('')
-    setValor('')
-    setFoto('')
-  };
-
-  return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Nome do Produto"
-        value={produto}
-        onChangeText={setProduto}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Descrição"
-        value={descricao}
-        onChangeText={setDescricao}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Valor"
-        value={valor}
-        onChangeText={setValor}
-        keyboardType="numeric"
-      />
-      <Button title="Escolher Foto" onPress={escolherFotoDaGaleria} />
-      {foto && <Image source={{ uri: foto }} style={styles.image} />}
-      <Button title="Cadastrar Produto" onPress={cadastrarProduto} />
-    </View>
+  return(
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Produto" component={Produto} />
+      <Stack.Screen name="Cadastro" component={CadastroUsuario} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    width: '80%',
-    marginBottom: 10,
-    padding: 10,
-  },
-  image: {
-    width: 200,
-    height: 200,
-    marginBottom: 10,
-  },
-});
