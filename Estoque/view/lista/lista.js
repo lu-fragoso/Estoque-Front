0ba9595f4d8aadc5e727adb820a5059dcc6f7d8d
Lucas Fragoso <88888171+lu-fragoso@users.noreply.github.com>
2024-04-22 19:49:00 -0300

    Corrigir importação de dependências e estilos na tela de lista de produtos

--- Estoque/view/lista/lista.js
+++ Estoque/view/lista/lista.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { FlatList, Text, View, StyleSheet } from 'react-native';
+import { FlatList, Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import axios from 'axios';
 
-const ListaProdutos = () => {
+const ListaProdutos = ({ navigation }) => {
   const [produtos, setProdutos] = useState([]);
 
   useEffect(() => {
-    axios.get('http://192.168.1.106:3000/product')
+    axios.get('http://172.17.112.215:3000/product')
       .then(response => {
         setProdutos(response.data);
       })
@@ -17,17 +17,25 @@ const ListaProdutos = () => {
 
   return (
     <View styles={styles.container}>
+      
       <FlatList
         data={produtos}
         keyExtractor={item => item.id.toString()}
         renderItem={({ item }) => (
-          <View  style={styles.flat}>
-            <Text>{item.nome}</Text>
-            <Text>{item.descricao}</Text>
-            <Text>{item.valor}</Text>
-          </View>
-        )}
+          <TouchableOpacity onPress={() => navigation.navigate('Detalhes', { produto: item })}>
+            <View  style={styles.flat}>
+              <Text>{item.nome}</Text>
+              <Text>{item.descricao}</Text>
+              <Text>{item.valor}</Text>
+            </View>
+          </TouchableOpacity>
+      )}
       />
+
+      <TouchableOpacity style={styles.newProduct} onPress={() => navigation.navigate('Produto')}>
+        <Text style={styles.iconButton}>+</Text>
+      </TouchableOpacity>
+
     </View>
   );
 };
@@ -36,14 +44,32 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+      paddingTop:20,
     },
     flat: {
-        flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
         padding: 10,
+        margin:5,
+        backgroundColor: '#B1B1B1',
+    },
+
+    iconButton: {
+      fontSize: 25,
+      color: '#ffffff',
+      fontWeight: 'bold',
+    },
+
+    newProduct: {
+      position: 'absolute',
+      bottom:30,
+      left: 30,
+      width: 50,
+      height: 50,
+      borderRadius: 50,
+      backgroundColor: '#000000',
+      alignItems: 'center',
+      justifyContent: 'center',
     },
   });
   
